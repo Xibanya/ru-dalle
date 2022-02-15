e45cfbf219f54ce127a801d791c5717b46c195fd
Manuela Malasaña <Xibanya@users.noreply.github.com>
2022-02-15 09:43:31 -0600

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import datetime
+
 import gc
 import pathlib
+import warnings
 from pathlib import Path
 
 import torch
 import torch.nn.functional as F
 from pynvml import *
 from tqdm import tqdm
+from translate import Translator
 
 from colors import print_cyan
-from rudalle import get_rudalle_model, get_tokenizer, get_vae
-from rudalle.pipelines import generate_images
+from rudalle import get_rudalle_model, get_tokenizer, get_vae, get_realesrgan
+from rudalle.pipelines import generate_images, super_resolution
 
+warnings.filterwarnings("ignore", category=UserWarning)
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = f"max_split_size_mb:2048"
 dev = torch.device('cuda')
 torch.nn.functional.conv2d(
@@ -21,14 +24,17 @@ torch.nn.functional.conv2d(
     torch.zeros(32, 32, 32, 32, device=dev)
 )
 
-input_text = u"город на закате"
-readable_caption = 'city at sunset'
+readable_caption = 'city at night'
+input_text = Translator(to_lang='ru').translate(readable_caption)
+
 TOP_P = 0.999
 TOP_K = 2048
 TEMPERATURE = 1.0
-CHECKPOINT_PATH = 'checkpoints/rudalle_dalle_last.pt'
-OUTPUT_PATH = 'content/output'
-IMAGE_COUNT = 4
+CHECKPOINT_PATH = 'checkpoints/city-night.pt'
+OUTPUT_PATH = 'content/output/CityNight'
+IMAGE_COUNT = 2
+SUPER_RESOLUTION = True
+SR = 'x2'  # x8, x4, or x2
 
 model = get_rudalle_model('Malevich', pretrained=True, fp16=True, device='cuda')
 if Path.exists(Path(CHECKPOINT_PATH)):
@@ -36,10 +42,12 @@ if Path.exists(Path(CHECKPOINT_PATH)):
     print(f'Loaded from {CHECKPOINT_PATH}')
 vae = get_vae().to('cuda')
 tokenizer = get_tokenizer()
+realesrgan = get_realesrgan(SR, device='cuda') if SUPER_RESOLUTION else None
 
 output_path = Path(OUTPUT_PATH)
 output_path.mkdir(parents=True, exist_ok=True)
-
+gc.collect()
+torch.cuda.empty_cache()
 
 for i in tqdm(range(IMAGE_COUNT), colour='green'):
     pil_images, _ = generate_images(
@@ -51,13 +59,18 @@ for i in tqdm(range(IMAGE_COUNT), colour='green'):
         top_p=TOP_P,
         temperature=TEMPERATURE
     )
+    if SUPER_RESOLUTION:
+        pil_images = super_resolution(pil_images, realesrgan)
     caption = readable_caption if readable_caption is not None else input_text
-    save_name = f"{caption}_{str(i)}"
+    sr = '_' + SR if SUPER_RESOLUTION else ''
+    save_index = 0
+    save_name = f'{caption}_{save_index:03d}'
     save_path = pathlib.Path(os.path.join(output_path, save_name + '.png'))
-    if pathlib.Path.exists(save_path):
-        save_name = save_name + datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    while pathlib.Path.exists(save_path):
+        save_index = save_index + 1
+        save_name = f'{caption}_{save_index:03d}'
         save_path = pathlib.Path(os.path.join(output_path, save_name + '.png'))
-    print_cyan(f'saving {save_name}')
+    print_cyan(f'saving {save_path}')
     for n in range(len(pil_images)):
         pil_images[n].save(save_path)
 
--- make_grid.py
+++ make_grid.py
@@ -4,32 +4,62 @@ import os
 
 from PIL import Image
 import colors
+from rudalle import get_realesrgan
+from rudalle.pipelines import super_resolution
 
-FOLDER = 'content/output/Data'
+FOLDER = 'content/output/City'
 
 ROWS = 4
 COLS = 4
 SIZE = 256
 
-types = ('*.png', '*.jpg', "*.jpeg", "*.bmp")
-paths = []
-for ext in types:
-    paths.extend(glob.glob(os.path.join(FOLDER, ext)))
 
-colors.print_blue(f"{len(paths)} images found")
+def make_grid(folder: str = FOLDER,
+              rows: int = ROWS,
+              cols: int = COLS,
+              size: int = SIZE,
+              show: bool = False
+              ):
 
-new = Image.new("RGB", (SIZE * COLS, SIZE * ROWS))
+    paths = []
+    types = ('*.png', '*.jpg', "*.jpeg", "*.bmp")
+    for ext in types:
+        paths.extend(glob.glob(os.path.join(folder, ext)))
 
-for y in range(ROWS):
-    for x in range(COLS):
-        index = x + y * COLS
-        if index > len(paths) - 1:
-            index = index % len(paths)
-        img = Image.open(paths[index])
-        new.paste(img, (x * SIZE, y * SIZE))
+    colors.print_blue(f"{len(paths)} images found")
 
-now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-save_path = FOLDER + f'/grid_{ROWS}x{COLS}_{now}.png'
-new.save(save_path)
-colors.print_cyan(f'saved to {save_path}')
-new.show()
+    new = Image.new("RGB", (size * cols, size * rows))
+
+    for y in range(rows):
+        for x in range(cols):
+            index = x + y * cols
+            if index > len(paths) - 1:
+                index = index % len(paths)
+            img = Image.open(paths[index])
+            new.paste(img, (x * size, y * size))
+
+    now = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    save_path = folder + f'/grid_{rows}x{cols}_{now}.png'
+    new.save(save_path)
+    colors.print_cyan(f'saved to {save_path}')
+    if show:
+        new.show()
+
+
+def supersize_all(folder: str = FOLDER, model: str = 'x4'):
+    paths = []
+    images = []
+    types = ('*.png', '*.jpg', "*.jpeg", "*.bmp")
+    for ext in types:
+        paths.extend(glob.glob(os.path.join(folder, ext)))
+    colors.print_blue(f"{len(paths)} images found")
+    for path in paths:
+        images.append(Image.open(path))
+    realesrgan = get_realesrgan(model, device='cuda')
+    images = super_resolution(images, realesrgan)
+    for n in range(len(images)):
+        path = paths[n].replace('.png', '')
+        images[n].save(f'{path}_{model}.png')
+
+
+supersize_all()
